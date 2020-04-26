ba30836bbd3a432748c44eb963c662c30837e163
Mike <mikeavb@yandex.ru>
2020-04-26 23:56:18 +0300

    Fix according to comments

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -8,17 +8,14 @@ public class ArrayStorage {
     private static final int DEFAULT_CAPACITY = 10_000;
 
     private Resume[] storage;
-    private int capacity;
     private int size;
 
     public ArrayStorage() {
-        this.capacity = DEFAULT_CAPACITY;
         this.storage = new Resume[DEFAULT_CAPACITY];
         this.size = 0;
     }
 
     public ArrayStorage(int capacity) {
-        this.capacity = capacity;
         this.storage = new Resume[capacity];
         this.size = 0;
     }
@@ -31,41 +28,31 @@ public class ArrayStorage {
     }
 
     public void save(Resume r) {
-        int existingIndex = indexOf(r);
+        Objects.requireNonNull(r);
+        int existingIndex = indexOf(r.uuid);
         if (existingIndex != -1) {
             storage[existingIndex] = r;
         } else {
-            if (size == capacity) {
-                ensureCapacity();
-            }
+            ensureCapacity(size + 1);
             storage[size] = r;
             size++;
         }
     }
 
-    private int indexOf(Resume r) {
-        Objects.requireNonNull(r);
-        for (int i = 0; i < size; i++) {
-            if (r == storage[i] || storage[i].uuid.equals(r.uuid)) {
-                return i;
-            }
+    private void ensureCapacity(int requiredCapacity) {
+        if (requiredCapacity > storage.length) {
+            int oldCapacity = storage.length;
+            int newCapacity = oldCapacity + (oldCapacity >> 1);
+            storage = Arrays.copyOf(storage, newCapacity);
         }
-        return -1;
-    }
-
-    private void ensureCapacity() {
-        int newCapacity = capacity + (capacity >> 1);
-        storage = Arrays.copyOf(storage, newCapacity);
-        capacity = newCapacity;
     }
 
     public Resume get(String uuid) {
         int requiredIndex = indexOf(uuid);
         if (requiredIndex == -1) {
             return null;
-        } else {
-            return storage[requiredIndex];
         }
+        return storage[requiredIndex];
     }
 
     private int indexOf(String uuid) {
@@ -81,13 +68,11 @@ public class ArrayStorage {
     public void delete(String uuid) {
         int requiredIndex = indexOf(uuid);
         if (requiredIndex != -1) {
-            if (requiredIndex == size - 1) {
-                storage[requiredIndex] = null;
-            } else {
+            if (requiredIndex < size - 1) {
                 System.arraycopy(storage, requiredIndex + 1, storage, requiredIndex,
                         size - requiredIndex - 1);
-                storage[size - 1] = null;
             }
+            storage[size - 1] = null;
             size--;
         }
     }
